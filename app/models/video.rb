d8e14b1cc28258c968ab2c551724e9aab07706b1
thonguyen <nguyentantho1302@gmail.com>
2012-11-06 10:37:03 +0700

    second commit

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,9 +4,16 @@ class Video < ActiveRecord::Base
   
 
   def get_top_videos
-		client = YouTubeIt::Client.new(:dev_key => "AI39si74utp1SAhXpQ1Ib8cuM1TKS-yZybNqGBoYSeL4Hg0AQCjyq7jXn2dQea717_VPw8TC1SfHmZ2JOwpkuKLEA7aDg1tdcA")
-  	videos = client.videos_by(:user => "CTFxC", :time => :today)
+	client = YouTubeIt::Client.new(:dev_key => "AI39si74utp1SAhXpQ1Ib8cuM1TKS-yZybNqGBoYSeL4Hg0AQCjyq7jXn2dQea717_VPw8TC1SfHmZ2JOwpkuKLEA7aDg1tdcA")
+  	videos = client.videos_by(:top_rated, :time => :this_week, :user => "CTFxC")
+  end
 
+  def save_videos(video)
+  	@video = Video.find_or_create_by_video_id(video.video_id) do |v|
+  		v.video_id = video.video_id
+  		v.video_name = video.title
+  		v.save
+  	end
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121105065043) do
+ActiveRecord::Schema.define(:version => 20121106031513) do
 
   create_table "videos", :force => true do |t|
     t.integer  "this_week_rank"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20121105065043) do
     t.integer  "time_since_upload"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
+    t.string   "video_id"
   end
 
 end

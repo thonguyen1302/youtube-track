ae43878aa7cb4f15f4dc1f47394ae49694a7cc44
thonguyen <nguyentantho1302@gmail.com>
2012-11-08 11:47:21 +0700

    seven commit

--- app/models/video.rb
+++ app/models/video.rb
@@ -8,7 +8,13 @@ class Video < ActiveRecord::Base
 
   def get_top_playlists
 		client = YouTubeIt::Client.new(:dev_key => "AI39si74utp1SAhXpQ1Ib8cuM1TKS-yZybNqGBoYSeL4Hg0AQCjyq7jXn2dQea717_VPw8TC1SfHmZ2JOwpkuKLEA7aDg1tdcA")
-		playlists = client.playlists(:user => "TheEllenShow")
+		playlists = client.playlists("TheEllenShow")
+  end
+
+  def get_videos_of_playlists(playlist_id)
+		client = YouTubeIt::Client.new(:dev_key => "AI39si74utp1SAhXpQ1Ib8cuM1TKS-yZybNqGBoYSeL4Hg0AQCjyq7jXn2dQea717_VPw8TC1SfHmZ2JOwpkuKLEA7aDg1tdcA")
+		playlists = client.playlist(playlist_id)
+		playlists.videos
   end
 
 
@@ -19,13 +25,14 @@ class Video < ActiveRecord::Base
 		  if (clip.total_aggregate_view < video.view_count)
 		  	view_c = (video.view_count-(clip.total_aggregate_view))+view_c
 		  end
+		  last_week_r = clip.this_week_rank
 			  clip.update_attributes!( 
 			  	:video_id => video.video_id,
 			    :video_name => video.title,
 			    :this_week_rank => temp,
-			    :last_week_rank => temp,
+			    :last_week_rank => last_week_r,
 			    :this_week_view => view_c,
-			    :total_aggregate_view => video.view_count,		    
+			    :total_aggregate_view => video.view_count,
 			  	:time_since_upload => video.uploaded_at)
 		else
 			if Video.exists?(["video_id = ?", video.video_id])
@@ -35,7 +42,7 @@ class Video < ActiveRecord::Base
 			  	view_c = (video.view_count-(clip.total_aggregate_view))+view_c
 			  end
 			  last_week_r = clip.this_week_rank
-			  clip.update_attributes!( 
+			  clip.update_attributes!(
 				  	:video_id => video.video_id,
 				    :video_name => video.title,
 				    :this_week_rank => temp,
